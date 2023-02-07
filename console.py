035fc37a17d5552640393d11da3e7eeff45e5a4e
Manuel254 <immanuelkituku@gmail.com>
2023-02-07 09:12:07 -0800

    Added the base model class

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ to be used for interaction with objects
 import cmd
 import sys
 
+
 class Console(cmd.Cmd):
     """This is a console that processes commands"""
 
@@ -18,6 +19,7 @@ class Console(cmd.Cmd):
     def do_quit(self, line):
         """Quits the console"""
         sys.exit(0)
+
 
 if __name__ == '__main__':
     Console().cmdloop()
