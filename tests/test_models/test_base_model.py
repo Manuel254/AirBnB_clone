3580ec3caec6b585bd341acc18f7e0d24b1256c6
Manuel254 <immanuelkituku@gmail.com>
2023-02-12 07:08:58 -0800

    Updated tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,7 +4,7 @@ BaseModel
 """
 import unittest
 from models.base_model import BaseModel
-
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
     """This class defines tests cases for the BaseModel
@@ -26,7 +26,17 @@ class TestBaseModel(unittest.TestCase):
         """tests what the instance type is"""
         self.assertIsInstance(self.bm1, BaseModel)
         self.assertIsInstance(str(self.bm1), str)
+        self.assertEqual(str(self.bm2),
+                        "[BaseModel] ({}) {}".format(b.id, b.__dict__))
 
     def test_to_dict(self):
         """tests the dictionary representation of an instance"""
         self.assertIsInstance(self.bm2.to_dict(), dict)
+
+    def test_type_created_at(self):
+        """tests if type of created time attribute is datetime"""
+        self.assertIs(self.bm1.created_at, datetime)
+
+    def test_type_updated_at(self):
+        """tests if type of created time attribute is datetime"""
+        self.assertIs(self.bm1.updated_at, datetime)
